04501f51161ac154f2d980e563a22d8c205b3b71
shashankg361 <shashankg361@gmail.com>
2023-11-26 10:35:27 +0530

    button_css

--- pages/index.js
+++ pages/index.js
@@ -43,14 +43,14 @@ export default  function Home() {
     <main
       className={`flex min-h-screen flex-col items-center mb-2 p-24`}
     >
-      <h1 className="text-3xl font-semibold">Wellcome to Spoty !!</h1>
-      <Link className="m-5" href={'https://spoty-13.vercel.app/api/authLog'}><button className=" border-2 border-white pr-6 pl-6 pt-2 pb-2 rounded-full bg-green-500 text-2xl">Get Started</button></Link>
+      <h1 className="start-text text-3xl font-semibold">Wellcome to Spoty !!</h1>
+      <Link className="m-5" href={'https://spoty-13.vercel.app/api/authLog'}><button className="start-btn border-2 border-white pr-6 pl-6 pt-2 pb-2 rounded-full bg-green-500 text-2xl">Get Started</button></Link>
       <h1>Hello - {data ? data.data.display_name : 'Not loggedIn'}</h1>
       <div>
         {data && 
         ( currentSong ? 
         <iframe src={`https://open.spotify.com/embed/track/${currentSong}`} width="300" height="380"  allowtransparency="true" allow="encrypted-media"></iframe>
-        : <h1 className="text-xl m-3 font-semibold">Currently No song is been played !</h1>)}
+        : <h1 className="text-xl m-3 font-semibold">Currently No song is being played !</h1>)}
       </div>
       {
         data && 
